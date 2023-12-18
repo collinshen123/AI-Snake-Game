3c20fa19de421111ede51f8a904f2bd629cc4fa8
collinshen123 <collinshen123@gmail.com>
2023-12-18 15:20:55 -0800

    plotting changes

--- helper.py
+++ helper.py
@@ -10,10 +10,15 @@ def plot(scores, mean_scores):
     plt.title('Training...')
     plt.xlabel('Number of Games')
     plt.ylabel('Score')
-    plt.plot(scores)
-    plt.plot(mean_scores)
+    
+    plt.plot(scores, label='Scores')
+    plt.plot(mean_scores, label='Mean Scores')
+    
     plt.ylim(ymin=0)
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    
+    plt.text(len(scores)-1, scores[-1], f'{round(scores[-1], 2)}', ha='left', va='bottom')
+    plt.text(len(mean_scores)-1, mean_scores[-1], f'{round(mean_scores[-1], 2)}', ha='left', va='bottom')
+    
+    plt.legend()
     plt.show(block=False)
     plt.pause(.1)
